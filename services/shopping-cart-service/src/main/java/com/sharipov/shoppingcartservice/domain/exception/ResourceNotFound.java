4690fdb10b068bdfe67756b8d136c113c9e10516
IslomW <119986336+IslomW@users.noreply.github.com>
2024-11-13 11:04:51 +0900

    feat(ResourceNotFound.java) add exeption

--- services/shopping-cart-service/src/main/java/com/sharipov/shoppingcartservice/domain/exception/ResourceNotFound.java
+++ services/shopping-cart-service/src/main/java/com/sharipov/shoppingcartservice/domain/exception/ResourceNotFound.java
@@ -1,2 +1,7 @@
-package com.sharipov.shoppingcartservice.domain.exception;public class ResourceNotFound {
+package com.sharipov.shoppingcartservice.domain.exception;
+
+public class ResourceNotFound extends RuntimeException {
+    public ResourceNotFound(String message) {
+        super(message);
+    }
 }

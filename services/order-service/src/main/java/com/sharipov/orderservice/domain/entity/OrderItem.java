7e714c2e8718d726e3789975cba383ca8b96aaa0
IslomW <119986336+IslomW@users.noreply.github.com>
2024-11-21 23:06:06 +0900

    chore(OrderItem) add OrderItem

--- services/order-service/src/main/java/com/sharipov/orderservice/domain/entity/OrderItem.java
+++ services/order-service/src/main/java/com/sharipov/orderservice/domain/entity/OrderItem.java
@@ -1,4 +1,24 @@
 package com.sharipov.orderservice.domain.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long orderId; // ID of the associated order (to avoid direct mapping)
+
+    private Long productId; // ID of the product (retrieved from Product Service)
+
+    private int quantity;
+
+    private double price;
 }

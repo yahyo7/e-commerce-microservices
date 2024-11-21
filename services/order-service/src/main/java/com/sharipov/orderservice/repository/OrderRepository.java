721c44b1227275b61e6a1d7585d7b6990e58aa2e
IslomW <119986336+IslomW@users.noreply.github.com>
2024-11-21 23:15:12 +0900

    feat(OrderServiceImpl) add getOrder

--- services/order-service/src/main/java/com/sharipov/orderservice/repository/OrderRepository.java
+++ services/order-service/src/main/java/com/sharipov/orderservice/repository/OrderRepository.java
@@ -3,5 +3,7 @@ package com.sharipov.orderservice.repository;
 import com.sharipov.orderservice.domain.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 }
--- services/order-service/src/main/java/com/sharipov/orderservice/service/impl/OrderServiceImpl.java
+++ services/order-service/src/main/java/com/sharipov/orderservice/service/impl/OrderServiceImpl.java
@@ -1,2 +1,37 @@
-package com.sharipov.orderservice.service.impl;public class OrderServiceImpl {
+package com.sharipov.orderservice.service.impl;
+
+import com.sharipov.orderservice.domain.entity.OrderEntity;
+import com.sharipov.orderservice.domain.exception.ResourceNotFound;
+import com.sharipov.orderservice.repository.OrderRepository;
+import com.sharipov.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+
+    @Override
+    public OrderEntity getOrder(Long orderId) {
+        return orderRepository.findById(orderId).orElseThrow(
+                () -> new ResourceNotFound("Order not found.")
+        );
+    }
+
+    @Override
+    public OrderEntity createOrder(OrderEntity orderEntity) {
+        return null;
+    }
+
+    @Override
+    public OrderEntity updateOrder(OrderEntity orderEntity, Long orderId) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(Long orderId) {
+
+    }
 }

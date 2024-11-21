650f1f6ea0487a78824a7a70ff9da4225fa2501a
IslomW <119986336+IslomW@users.noreply.github.com>
2024-11-21 23:09:19 +0900

    fix(OrderService) fix methods

--- services/order-service/src/main/java/com/sharipov/orderservice/service/OrderService.java
+++ services/order-service/src/main/java/com/sharipov/orderservice/service/OrderService.java
@@ -1,5 +1,11 @@
 package com.sharipov.orderservice.service;
 
 
+import com.sharipov.orderservice.domain.entity.OrderEntity;
+
 public interface OrderService {
+OrderEntity getOrder(Long orderId);
+OrderEntity createOrder(OrderEntity orderEntity);
+OrderEntity updateOrder(OrderEntity orderEntity, Long orderId);
+void deleteOrder(Long orderId);
 }
